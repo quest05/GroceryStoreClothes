c0928f3bbdefcfffcccc53991475edd04d1f39d3
quest05 <lakhani0982@gmail.com>
2023-12-21 22:20:00 +0530

    minor change to adapter

--- app/src/main/java/com/example/grocerystoreclothes/MainActivity.kt
+++ app/src/main/java/com/example/grocerystoreclothes/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.grocerystoreclothes.view.MainViewModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.grocerystoreclothes.model.entity.StoreCategory
 import com.example.grocerystoreclothes.model.entity.StoreProduct
@@ -35,29 +36,37 @@ class MainActivity : AppCompatActivity() {
         val jsonStrStoreCat = inputStreamStoreCat.bufferedReader().readText()
 
         val storeCategoryListType = object : TypeToken<List<StoreCategory>>() {}.type
-        val storeCategories: List<StoreCategory> = Gson().fromJson(jsonStrStoreCat, storeCategoryListType)
+        val storeCategories: List<StoreCategory> =
+            Gson().fromJson(jsonStrStoreCat, storeCategoryListType)
 
         val storeSubCatListType = object : TypeToken<List<StoreSubCategory>>() {}.type
-        val storeSubCategories: List<StoreSubCategory> = Gson().fromJson(inputStreamStoreSubCat.bufferedReader().readText(), storeSubCatListType)
+        val storeSubCategories: List<StoreSubCategory> =
+            Gson().fromJson(inputStreamStoreSubCat.bufferedReader().readText(), storeSubCatListType)
 
         val storeProductListType = object : TypeToken<List<StoreProduct>>() {}.type
-        val storeProducts: List<StoreProduct> = Gson().fromJson(inputStreamStoreProduct.bufferedReader().readText(), storeProductListType)
+        val storeProducts: List<StoreProduct> = Gson().fromJson(
+            inputStreamStoreProduct.bufferedReader().readText(),
+            storeProductListType
+        )
 
-        Log.e("TAG", "onCreate: "+ storeProducts)
+        Log.e("TAG", "onCreate: " + storeProducts)
         mainViewModel.insertDataBase(storeCategories, storeSubCategories, storeProducts)
 
-        binding.recyclerCategory.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerCategory.layoutManager = LinearLayoutManager(this)
+        binding.recyclerSubCategory.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerProduct.layoutManager = GridLayoutManager(this, 2)
         mainViewModel.getAllCategory()
         mainViewModel.getAllSubCategory()
         mainViewModel.getAllProducts()
         mainViewModel.storeCategoryList.observe(this) {
-            binding.recyclerCategory.adapter  = CategoryAdapter(it)
+            binding.recyclerCategory.adapter = CategoryAdapter(it)
         }
         mainViewModel.storeSubCategoryList.observe(this) {
-            binding.recyclerSubCategory.adapter  = SubCategoryAdapter(it)
+            binding.recyclerSubCategory.adapter = SubCategoryAdapter(it)
         }
         mainViewModel.productsList.observe(this) {
-            binding.recyclerProduct.adapter  = ProductAdapter(it)
+            binding.recyclerProduct.adapter = ProductAdapter(it)
         }
 
     }
